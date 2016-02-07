eac33941d124b68aa606d9f2f5a9436c519f594b
PoiSon <git@yerl.cn>
2016-02-07 20:56:19 +0800

    Update macros

--- PSStream/macros/convenientmacros.h
+++ PSStream/macros/convenientmacros.h
@@ -1,6 +1,5 @@
 //
-//  PSConvenientMacro.h
-//  PSExtensions
+//  Macros for convenient
 //
 //  Created by PoiSon on 15/10/8.
 //  Copyright © 2015年 yerl. All rights reserved.
@@ -16,20 +15,20 @@
 
 
 #define doIf(condition, action) \
-   do {                         \
-      if(!!(condition)){        \
-         action;                \
-      }                         \
-   } while(0)
+    do {                         \
+        if(!!(condition)){        \
+        action;                \
+        }                         \
+    } while(0)
 
 //消除[NSObject performSelector:]引起的警告
 #define SuppressPerformSelectorLeakWarning(action)                        \
-   do {                                                                   \
-      _Pragma("clang diagnostic push")                                    \
-      _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-      action;                                                             \
-      _Pragma("clang diagnostic pop")                                     \
-   } while (0)
+    do {                                                                   \
+        _Pragma("clang diagnostic push")                                    \
+        _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+        action;                                                             \
+        _Pragma("clang diagnostic pop")                                     \
+    } while (0)
 
 #define PSEnumOption(enum, value, comment) enum = value
 #define PSExternString(key, comment) FOUNDATION_EXPORT NSString * const _Nonnull key;
